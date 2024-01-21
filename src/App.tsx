fb9793fed71179821b2b27ee487958ee205e0f68
Aditya Prawira <adityaprawira96@gmail.com>
2024-01-21 21:04:13 +1100

    chore: update scrollbar
    - Update scroll bar during page transition
    - update scroll bar in about page
    - update scroll bar in project details page

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ import CustomFooter from "./CustomFooter";
 import CustomParticle from "./CustomParticle";
 import Page from "./common/components/Page";
 import WorkExperienceDetails from "./WorkExperienceDetails";
-
+import PerfectScrollBar from "react-perfect-scrollbar";
 export default function App(): JSX.Element {
   function findProject(urlTitle: string) {
     return projects.find(function (project) {
@@ -46,102 +46,107 @@ export default function App(): JSX.Element {
           <Route
             render={({ location }) => (
               <TransitionGroup>
-                <CSSTransition
-                  key={location.key}
-                  classNames="page"
-                  timeout={1000}
-                >
-                  <Switch location={location}>
-                    <Route
-                      exact
-                      path="/"
-                      render={() => (
-                        <Page>
-                          <Home />
-                        </Page>
-                      )}
-                    />
-                    <Route
-                      exact
-                      path="/about"
-                      render={(routeProps) => (
-                        <Page>
-                          <About
-                            educations={educations}
-                            workExperiences={workExperiences}
-                          />
-                        </Page>
-                      )}
-                    />
-                    <Route
-                      exact
-                      path="/about/educations/:title"
-                      render={(routeProps) => (
-                        <Page>
-                          <EducationDetails
-                            education={findEducation(
-                              routeProps.match.params.title
-                            )}
-                            allEducations={educations}
-                            {...routeProps}
-                          />
-                        </Page>
-                      )}
-                    />
-                    <Route
-                      exact
-                      path="/about/work-experiences/:title"
-                      render={(routeProps) => (
-                        <Page>
-                          <WorkExperienceDetails
-                            workExperience={findWorkExperience(
-                              routeProps.match.params.title
-                            )}
-                            allWorkExperiences={workExperiences}
-                            {...routeProps}
-                          />
-                        </Page>
-                      )}
-                    />
-                    <Route
-                      exact
-                      path="/projects"
-                      render={(routeProps) => (
-                        <Page>
-                          <ProjectsTimeline
-                            projects={projects}
-                            {...routeProps}
-                          />
-                        </Page>
-                      )}
-                    />
-                    <Route
-                      exact
-                      path="/projects/:title"
-                      render={(routeProps) => (
-                        <Page>
-                          <ProjectDetails
-                            project={findProject(routeProps.match.params.title)}
-                            filteredProjects={filteredProjects(
-                              routeProps.match.params.title
-                            )}
-                            allProjects={projects}
-                            {...routeProps}
-                          />
-                        </Page>
-                      )}
-                    />
-                    <Route
-                      exact
-                      path="/artworks"
-                      render={(routeProps) => (
-                        <Page>
-                          <Artworks artworks={artworks} {...routeProps} />
-                        </Page>
-                      )}
-                    />
-                  </Switch>
-                </CSSTransition>
+                <PerfectScrollBar>
+                  {" "}
+                  <CSSTransition
+                    key={location.key}
+                    classNames="page"
+                    timeout={1000}
+                  >
+                    <Switch location={location}>
+                      <Route
+                        exact
+                        path="/"
+                        render={() => (
+                          <Page>
+                            <Home />
+                          </Page>
+                        )}
+                      />
+                      <Route
+                        exact
+                        path="/about"
+                        render={(routeProps) => (
+                          <Page>
+                            <About
+                              educations={educations}
+                              workExperiences={workExperiences}
+                            />
+                          </Page>
+                        )}
+                      />
+                      <Route
+                        exact
+                        path="/about/educations/:title"
+                        render={(routeProps) => (
+                          <Page>
+                            <EducationDetails
+                              education={findEducation(
+                                routeProps.match.params.title
+                              )}
+                              allEducations={educations}
+                              {...routeProps}
+                            />
+                          </Page>
+                        )}
+                      />
+                      <Route
+                        exact
+                        path="/about/work-experiences/:title"
+                        render={(routeProps) => (
+                          <Page>
+                            <WorkExperienceDetails
+                              workExperience={findWorkExperience(
+                                routeProps.match.params.title
+                              )}
+                              allWorkExperiences={workExperiences}
+                              {...routeProps}
+                            />
+                          </Page>
+                        )}
+                      />
+                      <Route
+                        exact
+                        path="/projects"
+                        render={(routeProps) => (
+                          <Page>
+                            <ProjectsTimeline
+                              projects={projects}
+                              {...routeProps}
+                            />
+                          </Page>
+                        )}
+                      />
+                      <Route
+                        exact
+                        path="/projects/:title"
+                        render={(routeProps) => (
+                          <Page>
+                            <ProjectDetails
+                              project={findProject(
+                                routeProps.match.params.title
+                              )}
+                              filteredProjects={filteredProjects(
+                                routeProps.match.params.title
+                              )}
+                              allProjects={projects}
+                              {...routeProps}
+                            />
+                          </Page>
+                        )}
+                      />
+                      <Route
+                        exact
+                        path="/artworks"
+                        render={(routeProps) => (
+                          <Page>
+                            <Artworks artworks={artworks} {...routeProps} />
+                          </Page>
+                        )}
+                      />
+                    </Switch>
+                  </CSSTransition>
+                </PerfectScrollBar>
               </TransitionGroup>
             )}
           />
--- src/ProjectDetails.js
+++ src/ProjectDetails.js
@@ -8,42 +8,45 @@ import Grid from "@mui/material/Grid";
 import { v4 as uuidv4 } from "uuid";
 import styles from "./styles/ProjectDetailsStyles";
 import DetailsNavigator from "./DetailsNavigator";
+import PerfectScrollBar from "react-perfect-scrollbar";
 
 function ProjectDetails({ classes, project, filteredProjects, allProjects }) {
   const { descriptions, startPeriod, endPeriod, image, title } = project;
   const type = "projects";
   return (
     <Box flex fill className={classes.root}>
-      <div className={classes.content}>
-        <Grid container spacing={3} className={classes.container}>
-          <Grid item xs={12} sm={7}>
-            <Typography>
-              <label>Project Title:</label> {title}
-            </Typography>
-            <br />
-            <Typography>
-              <label>Date:</label> {startPeriod} - {endPeriod}
-            </Typography>
-            <br />
-            <Typography>
-              <label>Descriptions:</label>
-            </Typography>
-            <ul>
-              {descriptions.map((d) => (
-                <li key={uuidv4()}>{d}</li>
-              ))}
-            </ul>
+      <PerfectScrollBar>
+        <div className={classes.content}>
+          <Grid container spacing={3} className={classes.container}>
+            <Grid item xs={12} sm={7}>
+              <Typography>
+                <label>Project Title:</label> {title}
+              </Typography>
+              <br />
+              <Typography>
+                <label>Date:</label> {startPeriod} - {endPeriod}
+              </Typography>
+              <br />
+              <Typography>
+                <label>Descriptions:</label>
+              </Typography>
+              <ul>
+                {descriptions.map((d) => (
+                  <li key={uuidv4()}>{d}</li>
+                ))}
+              </ul>
+            </Grid>
+            <Grid item xs={12} sm={5} className={classes.media}>
+              <img src={image} alt={title} />
+            </Grid>
           </Grid>
-          <Grid item xs={12} sm={5} className={classes.media}>
-            <img src={image} alt={title} />
-          </Grid>
-        </Grid>
-      </div>
-      <DetailsNavigator
-        elements={allProjects}
-        currentItemTitle={title}
-        type={type}
-      />
+        </div>
+        <DetailsNavigator
+          elements={allProjects}
+          currentItemTitle={title}
+          type={type}
+        />
+      </PerfectScrollBar>
     </Box>
   );
 }
